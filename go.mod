8170102893418b8bbc3a1aca3f6fff3919962097
Andrew Snodgrass <git@pointlander.info>
2023-05-21 15:32:49 -0500

    WIP fine tuning

--- go.mod
+++ go.mod
@@ -10,15 +10,27 @@ require (
 )
 
 require (
+	git.sr.ht/~sbinet/gg v0.4.1 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/blevesearch/bleve v1.0.14 // indirect
 	github.com/blevesearch/go-porterstemmer v1.0.3 // indirect
 	github.com/blevesearch/segment v0.9.0 // indirect
 	github.com/blevesearch/snowballstem v0.9.0 // indirect
+	github.com/go-fonts/liberation v0.3.1 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/pointlander/gradient v0.0.0-20230114050126-69977707af34 // indirect
 	github.com/pointlander/pagerank v0.0.0-20210619221740-830548a59275 // indirect
 	github.com/remyoudompheng/go-liblzma v0.0.0-20190506200333-81bf2d431b96 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
-	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/image v0.7.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	gonum.org/v1/plot v0.13.0 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -11,11 +11,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"math/rand"
 	"os"
 	"sort"
+	"strings"
+	"time"
 
 	"github.com/pointlander/compress"
+	"github.com/pointlander/gradient/tf32"
 	"github.com/pointlander/pagerank"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 
 	bolt "go.etcd.io/bbolt"
 )
@@ -78,6 +86,15 @@ type Result struct {
 	Symbols []float64
 	Output  []byte
 }
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.9
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.999
+	// Eta is the learning rate
+	Eta = .3
+)
 
 func main() {
 	flag.Parse()
@@ -368,6 +385,112 @@ func main() {
 	}
 	var squad Squad
 	err = json.Unmarshal(data, &squad)
+	if err != nil {
+		panic(err)
+	}
+
+	rnd := rand.New(rand.NewSource(1))
+
+	set := tf32.NewSet()
+	set.Add("w1", 256, 1024)
+	set.Add("b1", 1024, 1)
+	set.Add("w2", 2048, 256)
+	set.Add("b2", 256, 1)
+	for _, w := range set.Weights {
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float32, StateTotal)
+			for i := range w.States {
+				w.States[i] = make([]float32, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for i := 0; i < cap(w.X); i++ {
+			w.X = append(w.X, float32((2*rnd.Float64()-1)*factor))
+		}
+		w.States = make([][]float32, StateTotal)
+		for i := range w.States {
+			w.States[i] = make([]float32, len(w.X))
+		}
+	}
+
+	others := tf32.NewSet()
+	others.Add("inputs", 256, 100)
+	inputs := others.ByName["inputs"]
+	inputs.X = inputs.X[:cap(inputs.X)]
+
+	for i := range inputs.X {
+		inputs.X[i] = rnd.Float32()
+	}
+
+	l1 := tf32.Everett(tf32.Add(tf32.Mul(set.Get("w1"), others.Get("inputs")), set.Get("b1")))
+	l2 := tf32.Add(tf32.Mul(set.Get("w2"), l1), set.Get("b2"))
+	cost := tf32.Sum(tf32.Quadratic(others.Get("inputs"), l2))
+
+	i := 1
+	pow := func(x float32) float32 {
+		y := math.Pow(float64(x), float64(i))
+		if math.IsNaN(y) || math.IsInf(y, 0) {
+			return 0
+		}
+		return float32(y)
+	}
+	points := make(plotter.XYs, 0, 8)
+	// The stochastic gradient descent loop
+	for i < 100 {
+		start := time.Now()
+		// Calculate the gradients
+		total := tf32.Gradient(cost).X[0]
+
+		// Update the point weights with the partial derivatives using adam
+		b1, b2 := pow(B1), pow(B2)
+		for j, w := range set.Weights {
+			for k, d := range w.D {
+				g := d
+				m := B1*w.States[StateM][k] + (1-B1)*g
+				v := B2*w.States[StateV][k] + (1-B2)*g*g
+				w.States[StateM][k] = m
+				w.States[StateV][k] = v
+				mhat := m / (1 - b1)
+				vhat := v / (1 - b2)
+				set.Weights[j].X[k] -= Eta * mhat / (float32(math.Sqrt(float64(vhat))) + 1e-8)
+			}
+		}
+
+		// Housekeeping
+		end := time.Since(start)
+		fmt.Println(i, total, end)
+		set.Zero()
+
+		if math.IsNaN(float64(total)) {
+			fmt.Println(total)
+			break
+		}
+
+		set.Zero()
+		others.Zero()
+
+		points = append(points, plotter.XY{X: float64(i), Y: float64(total)})
+		i++
+	}
+
+	// Plot the cost
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "cost.png")
 	if err != nil {
 		panic(err)
 	}
